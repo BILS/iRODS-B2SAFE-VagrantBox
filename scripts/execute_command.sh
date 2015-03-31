48370edff639e11c06e1863d57eab12363850ce0
Samuel Lampa <samuel.lampa@gmail.com>
2015-03-31 23:22:25 +0200

    Create a generic script for running commands (needed for i-commands in Ansible)

--- scripts/execute_command.sh
+++ scripts/execute_command.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -l
-zoneName=$1
-
-flagfile=/tmp/sharedspace_created;
+cmd=$1
+flagfile=$2
 rm $flagfile
-icd /$zoneName
-imkdir replicate
-echo "Shared space created on $(date %Y-%m-%d, %H:%M:%S)" > $flagfile
+eval $cmd
+echo "Executed command $cmd on $(date +%Y-%m-%d, %H:%M:%S)" > $flagfile
